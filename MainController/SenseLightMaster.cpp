85ca1d38d65a1bd99e9b210c90222f57a08906da
Liam Kinne <Liam Kinne>
2016-08-29 11:42:14 +1000

    Forgotten .cpp file

---
+++ MainController/SenseLightMaster.cpp
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+#include "Wire.h"
+#include "SenseLightMaster.h"
+
+SenseLightMaster::SenseLightMaster() {}
+
+SenseLightMaster::~SenseLightMaster() { /* NOTHING TO DECONSTRUCT */ }
+
+uint8_t SenseLightMaster::get() {
+	return _value;
+}
+
+void SenseLightMaster::set(uint8_t value) {
+	_value = value;
+}
