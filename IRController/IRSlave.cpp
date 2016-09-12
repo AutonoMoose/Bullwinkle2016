27e9e485d071177bd7ca5ef7277e4d010045e648
Liam Kinne <kinneliam@gmail.com>
2016-09-12 21:15:35 +1000

    Added refresh functionality
    not tested due to hardware issues.

--- IRController/IRSlave.cpp
+++ IRController/IRSlave.cpp
@@ -2,21 +2,44 @@
 #include "IRSlave.h"
 
 IRSlave::IRSlave(uint8_t powerPin, uint8_t clockPin, uint8_t latchPin, uint8_t dataPin, uint8_t sensorCount): _powerPin(powerPin), _clockPin(clockPin), _latchPin(latchPin), _dataPin(dataPin), _sensorCount(sensorCount) {
-
+	_byteCount = ceil(_sensorCount / 8);
+	uint _bitArray[_sensorCount];
 }
 
-IRSlave::~IRSlave() {
+IRSlave::~IRSlave() { /* NOTHING TO DECONSTRUCT */ }
 
-}
+void IRSlave::refresh() {
+	// Power Cycle
+	digitalWrite(_powerPin, LOW);
+	delay(2);
+	digitalWrite(_powerPin, HIGH);
 
-uint8_t IRSlave::getBest() {
-	return _best;
-}
+	// Shift Data In
+	for (uint pulse = 0; pulse < _maxPulseLength; ++pulse) {
+		// Latching
+		digitalWrite(_clockPin, HIGH); // Set Clock State
+		digitalWrite(_latchPin, LOW);
+		digitalWrite(_latchPin, HIGH);
 
-uint8_t IRSlave::getAngle() {
-	return _angle;
-}
+		// Bit Shifting
+		for (uint byte = 0; byte < _byteCount; ++_byteCount) {
+			uint8_t currentByte = shiftIn(_dataPin, _clockPin, MSBFIRST);
 
-IRSlave::refresh() {
+			for (uint bit = 0; bit < 8; ++bit) {
+				if (bit + (byte * 8) > _sensorCount) {
+					_bitArray[bit + (byte * 8)] = bitRead(currentByte, bit);	
+				}
+				else { break; }
+			}
+		}
+	}
 
+	_best = 0; // Reset best
+
+	// Find Best
+	for (uint i = 0; i < _sensorCount; i++) {
+		if (_bitArray[i] > _best) {
+			_best = i;
+		}
+	}
 }
--- IRController/IRSlave.h
+++ IRController/IRSlave.h
@@ -6,20 +6,24 @@ public:
 	IRSlave(uint8_t powerPin, uint8_t clockPin, uint8_t latchPin, uint8_t dataPin, uint8_t sensorCount);
 	~IRSlave();
 
-	uint8_t getBest();
-	uint8_t getAngle();
+	uint8_t getBest() { return _best; }
 
 	void refresh();
 private:
+	// Pin Configuration
 	uint8_t _powerPin;
 	uint8_t _clockPin;
 	uint8_t _latchPin;
 	uint8_t _dataPin;
 
-	uint8_t _sensorCount;
+	// Shift-In Configuration
+	uint _sensorCount;
+	uint _maxPulseLength = 255;
+	uint _byteCount;
+	uint _bitArray[];
 
+	// Outputs
 	uint8_t _best;
-	uint8_t _angle;
 };
 
 #endif /* IRSLAVE_H */
